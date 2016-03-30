0bbd838486971cdef82352f2b07f97d5c0f76922
edwin schleret <edwin.schleret@epitech.eu>
2016-03-30 17:31:44 +0200

    Add exercises assosiationonetoone

--- src/engine/exercises/exAssociationOneToOne.js
+++ src/engine/exercises/exAssociationOneToOne.js
@@ -2,6 +2,9 @@ export default class ExAssociationOneToOne {
     constructor(stage, data) {
         this.stage = stage;
         this.data = data;
+        this.elementToFind = null;
+        this.possibility = null;
+        this.errors = [];
         this.checkData();
         this.test();
         console.log("ExAssociationOneToOne");
