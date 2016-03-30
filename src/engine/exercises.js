0f4e88c6e351167b08ee558e63ae43b17f4d74c9
edwin schleret <edwin.schleret@epitech.eu>
2016-03-30 15:55:18 +0200

    add class exercises

---
+++ src/engine/exercises.js
@@ -0,0 +1,21 @@
+import ExAssociationOneToOne from './exercises/exAssociationOneToOne'
+
+export default class Exercises {
+    constructor(stage, dataExo) {
+        this.stage = stage;
+        this.dataExo = dataExo;
+        this.exercises = null;
+        this.checkExercises();
+    }
+
+    checkExercises () {
+        console.log(this.dataExo.type);
+        switch (this.dataExo.type)
+        {
+            case "associateonetoone":
+                console.log("Association");
+                this.exercises = new ExAssociationOneToOne(this.stage, this.dataExo);
+                break;
+        }
+    }
+}
---
+++ src/engine/exercises/exAssociationOneToOne.js
@@ -0,0 +1,20 @@
+export default class ExAssociationOneToOne {
+    constructor(stage, data) {
+        this.stage = stage;
+        this.data = data;
+        this.checkData();
+        this.test();
+        console.log("ExAssociationOneToOne");
+    }
+
+    checkData() {
+    }
+
+    test() {
+        var bitmap = new createjs.Text("TEST");
+        this.stage.addChild(bitmap);
+
+        console.log("NEW TEST SHAPE");
+        this.stage.update();
+    }
+}
