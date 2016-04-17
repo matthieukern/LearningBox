9f85958606841111acee8e93b2d881a595c4f6a3
matthieu kern <oo-augur-oo@hotmail.fr>
2016-04-17 16:17:10 +0200

    Update Gallery

---
+++ src/components/screens/GalleryItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router'
+
+import MDLItem from '../material/components/MDLItem.jsx'
+import MDLButton from './../material/components/MDLButton.jsx'
+import MDLCard from './../material/components/MDLCard.jsx'
+
+var GalleryScreen = React.createClass({
+	render: function() {
+		const exercise = this.props.exercise;
+
+		return (
+			<MDLItem>
+				<b>{exercise.title}</b> <Link to={`/editor/${exercise.id}`}><MDLButton colored>Éditer</MDLButton></Link>
+			</MDLItem>
+		);
+	}
+});
+
+GalleryScreen.propTypes = {
+	exercise: React.PropTypes.object
+};
+
+export default GalleryScreen
